6bbcc4e40add87e9f24717b3f66a29a819efcb24
Lev <leviksun.s@gmail.com>
2025-08-05 12:59:52 +0300

    Исправлены оставшиеся ошибки синтаксиса

--- src/test/controllers/ManagersTest.java
+++ src/test/controllers/ManagersTest.java
@@ -17,15 +17,13 @@ class ManagersTest {
     void checkGetDefault() {
         Managers managers = new Managers();
         InMemoryTaskManager test = new InMemoryTaskManager();
-        assertEquals(test.toString(), managers.getDefault().toString(),
-                "Проверьте метод getDefault()");
+        assertEquals(test.toString(), managers.getDefault().toString(), "Проверьте метод getDefault()");
     }
 
     @Test
     void checkGetDefaultHistory() {
         Managers managers = new Managers();
         InMemoryHistoryManager test = new InMemoryHistoryManager();
-        assertEquals(test.getClass(), managers.getDefaultHistory().getClass(),
-                "Проверьте метод getDefaultHistory()");
+        assertEquals(test.getClass(), managers.getDefaultHistory().getClass(), "Проверьте метод getDefaultHistory()");
     }
 }
