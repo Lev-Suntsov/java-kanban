556b30e73a013fb5857d35ed1e7da30d096d9818
Lev <leviksun.s@gmail.com>
2025-11-04 23:10:46 +0300

    Финальное задание спринт 9

--- src/controllers/TaskHandler.java
+++
@@ -1,9 +0,0 @@
-package controllers;
-import com.sun.net.httpserver.HttpExchange;
-import java.io.IOException;
-import java.util.List;
-public class TaskHandler extends BaseHttpHandler{
-    public TaskHandler(TaskManager manager){
-        super(manager);
-    }
-}
